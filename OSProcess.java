e3316697d4f1f57f5246f310863e79a9b40f3e61
Sirchion <campbellc0797@gmail.com>
2025-09-19 10:56:25 +0100

    Main added

---
+++ OSProcess.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class OSProcess {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Enter software you use to open");
+            Scanner sc = new Scanner(System.in);
+            String userInput = sc.nextLine();
+            ProcessBuilder pb = new ProcessBuilder(userInput);
+            Process process = pb.start();
+
+            InputStream inputStream = process.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.err.println("Software does not exist");
+        }
+
+    }
+}
